4e9f8f2cfe2670b1c58f02a0b847662e07957929
PeterBarsa <peter.barsa@gmail.com>
2016-02-19 13:13:30 +0100

    new stuff

---
+++ MVVMDemo/LoadingEvents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMDemo
+{
+    public static class LoadingEvents
+    {
+
+
+        public static string GetMyProperty(DependencyObject obj)
+        {
+            return (string)obj.GetValue(MyLoadedEvent);
+        }
+
+        public static void SetMyProperty(DependencyObject obj, int value)
+        {
+            obj.SetValue(MyLoadedEvent, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyLoadedEvent =
+            DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(LoadingEvents), new PropertyMetadata(null, SetMyLoadedEvent));
+
+        private static void SetMyLoadedEvent(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            FrameworkElement element = d as FrameworkElement;
+            if(element != null)
+            {
+                element.Loaded += (s1, s2) =>
+                {
+                    var viewModel = element.DataContext;
+                    if (viewModel == null)
+                        return;
+                    var methodInfo = viewModel.GetType().GetMethod(e.NewValue.ToString());
+                };
+
+            }
+        }
+
+    }
+}
--- MVVMDemo/UserViewModel.cs
+++ MVVMDemo/UserViewModel.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Repositories;
+using Repositories.Divisions;
+using Repositories.Users;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +13,31 @@ namespace MVVMDemo
     public class UserViewModel : BindableBase
     {
 
+        private ObservableCollection<User> _userList;
+
+        private DivisionRepository _divisionRepository = new DivisionRepository();
+
+        private ObservableCollection<Division> _divisionList;
+
+        public ObservableCollection<Division> DivisionList { get { return _divisionList; } set { SetProperty(ref _divisionList, value); } }
+
+        private UserRepository _userRepository = new UserRepository();
+
+        public ObservableCollection<User> UserList { get { return _userList; } set { SetProperty(ref _userList, value); } }
+
         public UserViewModel()
         {
 
         }
 
+        public async void PopulateList()
+        {
+
+            var tempUserList = (List<User>)_userRepository.GetAll();
+            UserList = new ObservableCollection<User>(tempUserList);
+            var tempDivisionList = (List<Division>)_divisionRepository.GetAll();
+            DivisionList = new ObservableCollection<Division>(tempDivisionList);
+
+        }
     }
 }
